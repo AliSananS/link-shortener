3f93bcf0e8105a8980a6548b23a509e3bbcb34b0
Ali Sanan <alisanan9090@gmail.com>
2025-10-14 12:43:09 +0500

    Refactor: improved run scripts

--- frontend/src/siteData.ts
+++ frontend/src/siteData.ts
@@ -1,7 +1,8 @@
 const isDev = process.env.NODE_ENV === 'development';
+const url = process.env.WORKER_URL;
 const siteData = {
 	title: 'Link Shortener',
-	shortUrl: isDev ? 'http://localhost:8787' : 'https://link-shortener.alisanan9090.workers.dev',
+	shortUrl: isDev ? 'http://localhost:8787' : url ? url : 'https://links.alisanan9090.workers.dev',
 	github: 'https://github.com/AliSanan/link-shortener',
 } as const;
 
